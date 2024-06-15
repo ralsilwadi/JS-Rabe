f778c81566a4f81c4b97a221de328336531f3e16
ralsilwadi <ralsilwadi@gmail.com>
2024-06-15 05:31:15 -0500

    ...

--- js-my-stuff/class.js
+++ js-my-stuff/class.js
@@ -32,4 +32,58 @@ const firstDuplicate = arr => arr.find((elem) => arr.indexOf(elem) !== arr.lastI
 
 const getDuplicates = arr => [...new Set(arr.filter((elem) => arr.indexOf(elem) !== arr.lastIndexOf(elem)))] ?? -1
 
-console.log(getDuplicates(['A', 'foo', '12' , 12, 'bar', 'a', 'a', 'foo' ]))
+const reverseStringWords = str => str.trim().split(' ').map((elem) => elem.split('').reverse().join('')).join(' ')
+
+const add = (arr1, arr2) => {
+  if (arr2.length > arr1.length) [arr1, arr2] = [arr2, arr1]
+  return arr1.map((elem, index) => elem += (arr2[index] || 0))
+}
+
+const fizzBuzz = num => {
+  let result = []
+  for (let i = 1; i <= num; i++) {
+    if (i % 3 === 0 && i % 5 === 0) result.push('FizzBuzz')
+    else if (i % 3 === 0) result.push('Fizz')
+    else if (i % 5 === 0) result.push('Buzz')
+    else result.push(i) 
+  }
+  return result
+}
+
+
+const isPalindrome = str => str.toLowerCase() === str.toLowerCase().split('').reverse().join('')
+
+const removeDuplicates = arr => [...new Set(arr)]
+
+const isPrime = num => {
+  if (num < 2) return false
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) return false
+  }
+  return true
+}
+
+const arrFactorial = arr => arr.map(factorial)
+
+const findBiggestNumber = str => {
+  let result = ''
+  for (let char of str) {
+    if (char >= '0' && char <= '9') result += char
+    else result += ' '
+  }
+  result = result.split(' ')
+  return Math.max(...result)
+}
+
+
+const findSumNumbers = str => {
+  let result = ''
+  for (let char of str) {
+    if (char >= '0' && char <= '9') result += char
+    else result += ' '
+  }
+  result = result.split(' ')
+  return result.reduce((sum, elem) => sum + Number(elem) , 0)
+}
+
+const 
